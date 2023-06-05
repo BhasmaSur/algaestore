d91a104412bb88018f49764278bf8b2c6210e78a
mradulmishra <rakshashltd@gmail.com>
2023-06-06 01:08:15 +0530

    stats

--- app/page.js
+++ app/page.js
@@ -1,13 +1,8 @@
 import { Footer, Navbar } from '../components';
 import {
   About,
-  Explore,
-  Feedback,
-  GetStarted,
   Hero,
-  Insights,
-  WhatsNew,
-  World,
+  Stats,
 } from '../sections';
 
 const Page = () => (
@@ -17,19 +12,8 @@ const Page = () => (
     <div className="relative">
       <About />
       <div className="gradient-03 z-0" />
-      <Explore />
+      <Stats/>
     </div>
-    {/* <div className="relative">
-      <GetStarted />
-      <div className="gradient-04 z-0" />
-      <WhatsNew />
-    </div>
-    <World />
-    <div className="relative">
-      <Insights />
-      <div className="gradient-04 z-0" />
-      <Feedback />
-    </div> */}
     <Footer />
   </div>
 );
--- sections/About.jsx
+++ sections/About.jsx
@@ -23,7 +23,6 @@ const About = () => (
         className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       >
         <span className="font-extrabold text-white">Algae Store </span> is a comprehensive platform dedicated to facilitating the buying and selling of <span className="font-extrabold text-white"> Seaweed </span>products between <span className="font-extrabold text-white">Farmers </span>and <span className="font-extrabold text-white">Sellers</span> . Our mission is to connect seaweed farmers with interested buyers , creating a sustainable marketplace for this unique and valuable resource . To ensure a smooth and efficient experience for all users, we have developed various features and functionalities that cater to the specific needs of our community.<br /><br />
-        Let's explore the key aspects of our platform :
         {/* <span className="font-extrabold text-white">
           {' '}
           madness of the metaverse{' '}
@@ -34,13 +33,6 @@ const About = () => (
         metaverse world you want, turn your dreams into reality. Let's explore
         the madness of the meaverse by scrolling down. */}
       </motion.p>
-
-      <motion.img
-        variants={fadeIn('up', 'tween', 0.3, 1)}
-        src="/arrow-down.svg"
-        alt="arrow down"
-        className="w-[18px] h-[28px] object-contain mt-[28px]"
-      />
     </motion.div>
   </section>
 );
