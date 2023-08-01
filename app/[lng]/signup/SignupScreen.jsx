042f6294f5778c8db4fddca720cb5a0b501b1bce
akhilomar <akhilomar555@gmail.com>
2023-08-02 00:31:05 +0530

    sigup form

--- app/[lng]/signup/SignupScreen.jsx
+++ app/[lng]/signup/SignupScreen.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import { useSelector } from 'react-redux';
-import { useRouter } from 'next/navigation';
 
 
-const LoginScreen = () => {
+const SignupScreen = () => {
     const currentscreen = useSelector((store) => store.cart.loginscreen)
     console.log("LoginScreen", currentscreen)
-    const router = useRouter();
-    const handleSignup = () => {
-        router.push('/signup');
-    }
-
     return (
         <div>
             <section class="gradient-form h-full bg-neutral-20">
@@ -36,18 +30,30 @@ const LoginScreen = () => {
                                             </div>
 
                                             <form>
-                                                <p class="mb-4">Please login to your {currentscreen} account</p>
+                                                <p class="mb-4">Create your {currentscreen} account</p>
                                                 {/* <!--Username input--> */}
                                                 <div class="relative mb-4" data-te-input-wrapper-init>
                                                     <input
                                                         type="text"
                                                         class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                                                         // id="exampleFormControlInput1"
-                                                        placeholder="Username" />
+                                                        placeholder="Full Name" />
                                                     <label
                                                         for="exampleFormControlInput1"
                                                         class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                                                    >Username
+                                                    >Full Name
+                                                    </label>
+                                                </div>
+                                                <div class="relative mb-4" data-te-input-wrapper-init>
+                                                    <input
+                                                        type="email"
+                                                        class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                                                        // id="exampleFormControlInput1"
+                                                        placeholder="Email" />
+                                                    <label
+                                                        for="exampleFormControlInput1"
+                                                        class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                                                    >Email
                                                     </label>
                                                 </div>
 
@@ -64,6 +70,18 @@ const LoginScreen = () => {
                                                     >Password
                                                     </label>
                                                 </div>
+                                                <div class="relative mb-4" data-te-input-wrapper-init>
+                                                    <input
+                                                        type="password"
+                                                        class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                                                        // id="exampleFormControlInput11"
+                                                        placeholder="Confirm Password" />
+                                                    <label
+                                                        for="exampleFormControlInput11"
+                                                        class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                                                    >Confirm Password
+                                                    </label>
+                                                </div>
 
                                                 {/* <!--Submit button--> */}
                                                 <div class="mb-12 pb-1 pt-1 text-center">
@@ -76,23 +94,22 @@ const LoginScreen = () => {
                                                             background: 'linear-gradient(to right, #6D6CC7, #8B4BC7, #9542C7, #b44593)'
                                                         }}
                                                     >
-                                                        Log in
+                                                        Create Account
                                                     </button>
 
                                                     {/* <!--Forgot password link--> */}
-                                                    <a href="#!">Forgot password?</a>
+                                                    {/* <a href="#!">Forgot password?</a> */}
                                                 </div>
 
                                                 {/* <!--Register button--> */}
                                                 <div class="flex items-center justify-between pb-6">
-                                                    <p class="mb-0 mr-2">Don't have an account?</p>
+                                                    <p class="mb-0 mr-2">Already have an account?</p>
                                                     <button
-                                                        onClick={handleSignup}
                                                         type="button"
                                                         class="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
                                                         data-te-ripple-init
                                                         data-te-ripple-color="light">
-                                                        Register
+                                                        Login
                                                     </button>
                                                 </div>
                                             </form>
@@ -127,4 +144,4 @@ const LoginScreen = () => {
     );
 }
 
-export default LoginScreen;
+export default SignupScreen;
