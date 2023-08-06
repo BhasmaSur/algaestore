bc5719153f7964d40f51644f5cdaea1196fd3860
Mradul Mishra <72976792+BhasmaSur@users.noreply.github.com>
2023-08-07 02:12:08 +0530

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -9,30 +9,23 @@ export const corsHeaders = {
 export default async function handler(req, res) {
    const { method } = req;
 
-  // This will allow OPTIONS request
-  if (method === "OPTIONS") {
-    return res.status(200).send("ok");
-  }
   if (!req.body) {
     res.statusCode = 404;
     res.end('Error');
     return;
   }
-
-  // const { username, password } = req.body;
-  // if (username && password) {
-  //   authenticateUser({
-  //     username,
-  //     password,
-  //   }).then((src) => {
-  //     if (src) {
-  //       res.json({
-  //         jwtToken: src.jwtToken,
-  //         type : src.type
-  //       });
-  //     }
-  //   });
-  // }
-  res.setHeader(corsHeaders);
-  res.json({ foo: "bar" });
+  const { username, password } = req.body;
+  if (username && password) {
+    authenticateUser({
+      username,
+      password,
+    }).then((src) => {
+      if (src) {
+        res.json({
+          jwtToken: src.jwtToken,
+          type : src.type
+        });
+      }
+    });
+  }
 }
