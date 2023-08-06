c92bdbd9b37523510fb618d47160685351ee5fa5
Mradul Mishra <72976792+BhasmaSur@users.noreply.github.com>
2023-08-07 01:27:42 +0530

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,6 +1,12 @@
 import { authenticateUser } from '../../app/services/auth';
 
 export default async function (req, res) {
+   const { method } = req;
+
+  // This will allow OPTIONS request
+  if (method === "OPTIONS") {
+    return res.status(200).send("ok");
+  }
   if (!req.body) {
     res.statusCode = 404;
     res.end('Error');
