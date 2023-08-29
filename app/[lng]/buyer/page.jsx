a309024bbb212d32a48267908b2c217ed9925a83
mradulmishra <rakshashltd@gmail.com>
2023-08-29 22:13:48 +0530

    fix2

--- app/[lng]/buyer/page.jsx
+++ app/[lng]/buyer/page.jsx
@@ -49,17 +49,17 @@ const page = () => {
                             <div className='flex flex-col md:flex-row'>
                                 <div className="mb-2 md:mb-0 md:mr-4">
                                     <label htmlFor="email"  className="block font-medium mb-2">Email:</label>
-                                    <input type="email" id="email" placeholder='example@email.com' className="bg-gray-100 appearance-none border-2 border-gray-200 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" />
+                                    <input type="email" id="email" placeholder='example@email.com' className="bg-gray-100 appearance-none border-2 border-gray-200 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"/>
                                 </div>
                                 <div className="md:ml-4">
                                     <label htmlFor="phone" className="block font-medium mb-2">Phone number:</label>
-                                    <input type="text" id="phone" placeholder='+91 (XXX) XXX-XXXX' className="bg-gray-100 appearance-none border-2 border-gray-200 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="number" />
+                                    <input type="text" id="phone" placeholder='+91 (XXX) XXX-XXXX' className="bg-gray-100 appearance-none border-2 border-gray-200 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"/>
                                 </div>
                             </div>
                         </div>
                         <div className="mb-4">
                             <label htmlFor="industry" className="block mb-2 font-medium">Industry:</label>
-                            <select id="industry" className="bg-gray-100 appearance-none border-2 border-gray-100 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text">
+                            <select id="industry" className="bg-gray-100 appearance-none border-2 border-gray-100 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text">
                                 <option value="agriculture">Agriculture</option>
                                 <option value="foodAndBeverage">Food and Beverage</option>
                                 <option value="cosmetics">Cosmetics</option>
@@ -70,7 +70,7 @@ const page = () => {
                         </div>
                         <div className="mb-4">
                             <label htmlFor="additionalInfo" className="block mb-2 font-medium">Additional Info:</label>
-                            <textarea id="additionalInfo" placeholder="Tell us more about what you're doing!" className="w-full bg-gray-100 appearance-none border-2 border-gray-200 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text"></textarea>
+                            <textarea id="additionalInfo" placeholder="Tell us more about what you're doing!" className="w-full bg-gray-100 appearance-none border-2 border-gray-200 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text"></textarea>
                         </div>
                         <button type="submit" className="w-full px-4 py-2 bg-violet-500 text-white rounded hover:bg-violet-600">Send</button>
                     </form>
