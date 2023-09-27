919e0bd5d39eb5869967871315bcf0f71c73a53d
mradulmishra <rakshashltd@gmail.com>
2023-09-27 18:02:45 +0530

    emailjs

--- app/[lng]/checkout/checkoutItem.jsx
+++ app/[lng]/checkout/checkoutItem.jsx
@@ -5,9 +5,11 @@ import CheckourCard from './CheckoutCard';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { getUserDetailsFromCookie } from '../../services/auth';
 import { USER_BUYER_ROLE } from '../../constants/userConstants';
+import { sendEmail } from '../../services/emailService';
 
 const CheckoutItem = () => {
   const searchParams = useSearchParams();
+  const [optionalEmail, setOptionalEmail] = useState();
   const cartItems = JSON.parse(localStorage.getItem('cart-items'));
   const router = useRouter();
   const [isLoggin, setIsLoggedIn] = useState(false);
@@ -23,9 +25,13 @@ const CheckoutItem = () => {
   };
 
   useEffect(() => {
-    const userDetails = getUserDetailsFromCookie();
-    if (userDetails && userDetails.role === USER_BUYER_ROLE) {
+    const userDetailsFromCookie = getUserDetailsFromCookie();
+    if (
+      userDetailsFromCookie &&
+      userDetailsFromCookie.role === USER_BUYER_ROLE
+    ) {
       setIsLoggedIn(true);
+      setOptionalEmail(userDetailsFromCookie.emailId);
     } else {
       setIsLoggedIn(false);
     }
@@ -35,10 +41,37 @@ const CheckoutItem = () => {
     router.push('/cart');
   };
 
+  const placeOrder = () => {
+    const emailToMail = optionalEmail ? optionalEmail : userDetails.emailId;
+    let message = '';
+    cartItems.map((item, index) => {
+      message = message.concat(`${index + 1}. ${item.name} - ${item.price} \n`);
+    });
+    message = message.concat(`\n\n\nTotal Cost : ${calculateTotal()}`);
+    const mailObject = {
+      from_name: 'Alage Store',
+      to_name: 'Buyer',
+      heading: 'Your Order Details : ',
+      item_message: message,
+      to_email: emailToMail,
+    };
+    sendEmail(mailObject).then((res) => {
+      if (res) {
+        alert(
+          'Your order is submitted successfully, our representative will contact you soon.'
+        );
+        router.push('/store');
+      }
+    });
+  };
+
   return (
     <>
+      (
       <div className="text-center py-4 bg-gray-100 dark:bg-gray-900 mt-20">
-        <p className="text-dark text-2xl lg:text-5xl lg:mb-0 pt-5 pb-10">Checkout:</p>
+        <p className="text-dark text-2xl lg:text-5xl lg:mb-0 pt-5 pb-10">
+          Checkout:
+        </p>
         <div className="flex items-center">
           <div className="container max-w-1 px-0 lg:px-5 mx-1">
             <div className="grid gap-7 grid-cols-1 lg:grid-cols-1">
@@ -68,9 +101,9 @@ const CheckoutItem = () => {
                     )}
                   </div>
                   <div className="flex-row items-left pt-1">
-                  <div className="text-black mt-10 mb-10 text-2xl">
-                          Purchase Memo
-                        </div>
+                    <div className="text-black mt-10 mb-10 text-2xl">
+                      Purchase Memo
+                    </div>
                     <table class="border-collapse border border-slate-400 ...">
                       <thead>
                         <tr>
@@ -113,6 +146,22 @@ const CheckoutItem = () => {
                         </tr>
                       </tbody>
                     </table>
+                    <div className="mb-4">
+                      <label
+                        htmlFor="country"
+                        className="block mb-2 font-medium"
+                      >
+                        Change Email (optional) :
+                      </label>
+                      <input
+                        onChange={(e) => setOptionalEmail(e.target.value)}
+                        value={optionalEmail}
+                        type="text"
+                        id="email"
+                        placeholder={optionalEmail}
+                        className="bg-gray-100 appearance-none border-2 border-gray-200 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                      />
+                    </div>
                     <div>
                       <button
                         class="bg-red-500 hover:bg-hover-600 text-white font-bold py-3 px-6 rounded m-4"
@@ -121,7 +170,7 @@ const CheckoutItem = () => {
                         Back
                       </button>
                       <button
-                        onClick={() => router.push('/checkout')}
+                        onClick={placeOrder}
                         class="bg-green-500 hover:bg-green-600 text-white font-bold py-3 px-6 rounded m-4"
                       >
                         Place Order
@@ -134,7 +183,6 @@ const CheckoutItem = () => {
           </div>
         </div>
       </div>
-
       {!cartItems && <div>cart item is empty</div>}
     </>
   );
